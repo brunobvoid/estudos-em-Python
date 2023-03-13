5df4554888fc3059a16f47638fd84af1cdc4460f
Bruno Bastos <115432374+brunobvoid@users.noreply.github.com>
2023-03-13 15:40:48 -0300

    ex081

--- pythonexercicios/ex081.py
+++ pythonexercicios/ex081.py
@@ -1,1 +1,16 @@
 #ler varios numeros até parar, mostrar quandos numeros foram digitados, descrescente e se tem 5 na lista
+l=[]
+k = 'S'
+while k in 'S':
+    n = int(input('Digite um número: '))
+    l.append(n)
+    k = str(input('Quer continuar? [S/N] ')).strip().upper()
+    while k not in 'SN':
+        k = str(input('Opção inválida, Quer continuar? [S/N] ')).strip().upper()
+print(f'Você digitou {len(l)} elementos.')
+l.sort(reverse=True)
+print(f'A lista em decrescente é {l}')
+if 5 in l:
+    print('O valor 5 foi encontrado na lista!')
+else:
+    print('O valor 5 não foi encontrado na lista!')
