d5e7fea79c00e0471dc6ba302a7f538fd6a1a098
Bruno Bastos <115432374+brunobvoid@users.noreply.github.com>
2023-03-13 11:20:41 -0300

    ex080

--- pythonexercicios/Aulas/aula17.py
+++ pythonexercicios/Aulas/aula17.py
@@ -1,21 +1,22 @@
-'''num = [2, 5, 7, 9]
-num[2] = 3
+num = [2, 5, 7, 9]
+#num[2] = 3
 num.append(7)
 #num.sort(reverse=True)
-num.insert(2, 0)
+num.insert(2, 4)
 num.pop(2)
 print(num)
 print(f'Essa Lista tem {len(num)} elementos.')
 
-l = []
-l.append(5)
-l.append(9)
-l.append(4)
-for c, v in enumerate(l):
-    print(f'Na posição {c+1} encontrei o valor {v}.')'''
 
-a = [2, 5, 7, 9]
-b = a[:]
-b.insert(4, a[3])
-print(f'Lista A: {a}')
-print(f'Lista B: {b}')
+#l = []
+#l.append(5)
+#l.append(9)
+#l.append(4)
+#for c, v in enumerate(l):
+#    print(f'Na posição {c+1} encontrei o valor {v}.')
+
+#a = [2, 5, 7, 9]
+#b = a[:]
+#b.insert(4, a[3])
+#print(f'Lista A: {a}')
+#print(f'Lista B: {b}')
--- pythonexercicios/ex080.py
+++ pythonexercicios/ex080.py
@@ -1,1 +1,16 @@
 #digitar 5 valores e adicionar na lista ja na posição correta de forma que fique crescente
+l = []
+for c in range(0, 5):
+    n = int(input('Digite um número: '))
+    if c == 0 or n > l[-1]:
+        l.append(n)
+        print(f'O número {n} foi adicionado no fim da fila...')
+    else:
+        p = 0
+        while p < len(l):
+            if n <= l[p]:
+                l.insert(p, n)
+                print(f'O valor {n} foi adicionado na posição {p}...')
+                break
+            p += 1
+print(l)
