02f006dd6f85159ab92a69a03b12b9e83b09f576
Bruno Bastos <115432374+brunobvoid@users.noreply.github.com>
2023-03-14 10:53:50 -0300

    ex083

--- pythonexercicios/ex083.py
+++ pythonexercicios/ex083.py
@@ -1,1 +1,16 @@
 #ler uma expressão matemática e analisar se esta valida a expressão
+s = []
+e = str(input('Digite aqui a expressão matemática: '))
+for v in e:
+    if v == '(':
+        s.append('(')
+    elif v == ')':
+        if len(s) > 0:
+            s.pop()
+        else:
+            s.append(')')
+            break
+if len(s) == 0:
+    print('Sua expressão é válida!')
+else:
+    print('Sua expressão é inválida!')
