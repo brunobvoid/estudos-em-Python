fe3f16e23b97408914ce7a23357105093b1ffd7b
Bruno Bastos <115432374+brunobvoid@users.noreply.github.com>
2023-03-21 11:24:02 -0300

    ex85 completo

--- pythonexercicios/ex085.py
+++ pythonexercicios/ex085.py
@@ -1,1 +1,14 @@
 #ler 7 valores, mostrar uma lista com 2 listas de valores pares e impares em ordem crescente
+princ = []
+par = []
+impar = []
+for c in range(0, 7):
+    princ.append(int(input('Digite um número: ')))
+    if princ[c] % 2 == 0:
+        par.append(princ[c])
+    if princ[c] % 2 == 1:
+        impar.append(princ[c])
+princ.clear()
+princ.append(sorted(par))
+princ.append(sorted(impar))
+print(f'A lista completa em ordem crescente separada em pares e impares é: {princ}')
