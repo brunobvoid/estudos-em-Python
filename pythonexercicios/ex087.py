b8e2908153b26582c982ceb1c52f59686202430f
Bruno Bastos <115432374+brunobvoid@users.noreply.github.com>
2023-03-21 16:39:15 -0300

    ex85 ex 87

--- pythonexercicios/ex087.py
+++ pythonexercicios/ex087.py
@@ -1,1 +1,12 @@
 #aprimorar o ex anterior mostrando a soma dos valores pares a soma dos valores da 3 coluna e o maior valor da segunda linha
+m = []
+l = 1
+c = 1
+for x in range(1, 10):
+    m.append(int(input(f'Digite o numero na posição {l} x {c}: ')))
+    c += 1
+    if c == 4:
+        l += 1
+        c = 1
+print(f' [{m[0]:^6} {m[1]:^6} {m[2]:^6}]\n', f'[{m[3]:^6} {m[4]:^6} {m[5]:^6}]\n', f'[{m[6]:^6} {m[7]:^6} {m[8]:^6}]')
+for x in range(1, 10):
