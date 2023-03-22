a84a0c4ec8c50eabb49d114a1179262900d8a962
Bruno Bastos <115432374+brunobvoid@users.noreply.github.com>
2023-03-22 11:43:57 -0300

    ex87 ex88

--- pythonexercicios/ex087.py
+++ pythonexercicios/ex087.py
@@ -1,12 +1,20 @@
 #aprimorar o ex anterior mostrando a soma dos valores pares a soma dos valores da 3 coluna e o maior valor da segunda linha
 m = []
-l = 1
-c = 1
-for x in range(1, 10):
+l = c = s = sc = maior = 0
+for x in range(0, 9):
     m.append(int(input(f'Digite o numero na posição {l} x {c}: ')))
+    if m[x] % 2 == 0:
+        s = s + m[x]
+    if c == 2:
+        sc = sc + m[x]
+    if l == 1:
+       if m[x] > maior:
+           maior = m[x]
     c += 1
-    if c == 4:
+    if c == 3:
         l += 1
-        c = 1
+        c = 0
 print(f' [{m[0]:^6} {m[1]:^6} {m[2]:^6}]\n', f'[{m[3]:^6} {m[4]:^6} {m[5]:^6}]\n', f'[{m[6]:^6} {m[7]:^6} {m[8]:^6}]')
-for x in range(1, 10):
+print(f'A soma dos valores pares é {s}')
+print(f'A soma dos valores da terceira coluna é {sc}')
+print(f'O maior valor da segunda linha é {maior}')
--- pythonexercicios/ex088.py
+++ pythonexercicios/ex088.py
@@ -1,1 +1,31 @@
 #fazer um programa que ajude em palpites da mega sena, perguntar qntos jogos quer, e sortear 6 numeros de 1 a 60 de acordo a quantidade de jogos
+from random import randint
+import time
+l = list()
+jogos = list()
+
+
+print('-='*20)
+print('     MEGA SENA     ')
+print('-='*20)
+n = int(input('Quantos jogos você quer gerar? '))
+t = 1
+
+while t <= n:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in l:
+            l.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    l.sort()
+    jogos.append(l[:])
+    l.clear()
+    t += 1
+print()
+print('-=' * 4, f'  SORTEANDO {n} JOGOS  ', '-=' * 4)
+for i, k in enumerate(jogos):
+    time.sleep(1)
+    print(f'Jogo {i+1}: {k}')
